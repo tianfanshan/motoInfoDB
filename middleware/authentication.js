12566a8d966b925a7c7d14ff881ecd8fd9246c53
laopengyou <stf30111994@gmail.com>
2022-05-29 11:04:08 +0200

    authentication,isAdmin

---
+++ middleware/authentication.js
@@ -0,0 +1,40 @@
+const { User, Token, Sequelize } = require('../models');
+const { Op } = Sequelize;
+const jwt = require('jsonwebtoken');
+const { jwt_secret } = require('../config/config.json')['development']
+
+const authentication = async(req,res,next)=>{
+    try {
+        const token = req.headers.authorization;
+        const payload = jwt.verify(token,jwt_secret);
+        const user = await User.findByPk(payload.id);
+        const tokenFound = await Token.findOne({
+            where:{
+                [Op.and]:[
+                    {UserId: user.id},
+                    {token:token}
+                ]
+            }
+        });
+        if(!tokenFound){
+            return res.status(401).send({message:'You are not authorized'});
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({error,message:'There was a problem with the token'})
+    }
+}
+
+const isAdmin = async(req,res,next)=>{
+    const admins=['admin'];
+    if(!admins.includes(req.user.role)){
+        return res.status(403).send({
+            message:"You do not have permission"
+        });
+    }
+    next()
+}
+
+module.exports = { authentication, isAdmin }
--- router/users.js
+++ router/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const UserController = require('../controller/UserController')
+const { authentication ,isAdmin } = require('../middleware/authentication')
 
 router.post('/',UserController.create);
 router.post('/login',UserController.login);
